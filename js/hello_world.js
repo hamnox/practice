1f3bbd48b760942a6ba200e5ebc1fe45d0f2a38b
Melanie Heisey <mheisey.nox@gmail.com>
2015-08-04 08:45:42 -0700

    js: hello world (for web)

---
+++ js/hello_world.js
@@ -0,0 +1,5 @@
+var myHeading = document.querySelector('h1');
+
+myHeading.innerHTML = 'Hello World!';
+// print('Bother Bother Bother Bother Bother World.');
+alert('Bother Bother Bother Bother Bother'))
